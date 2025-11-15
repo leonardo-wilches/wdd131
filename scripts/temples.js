c86b121d4f2b11e4936aafe3c450dcc3085f63fb
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-11-15 14:55:19 -0300

    w2 progress

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const btn = document.getElementById("menuBtn");
+const navList = document.getElementById("navList");
+
+btn.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    btn.textContent = navList.classList.contains("open") ? "✖" : "☰";
+});
